6ad4762bf37a9816646e4dd3fffc87a876a7287c
Harloom <harloom.developer@gmail.com>
2019-12-17 23:41:25 +0700

    1.profile

--- app/src/main/java/com/overdrive/sedekahsampah/ui/user/UserActivity.kt
+++ app/src/main/java/com/overdrive/sedekahsampah/ui/user/UserActivity.kt
@@ -3,8 +3,11 @@ package com.overdrive.sedekahsampah.ui.user
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
+import androidx.core.os.bundleOf
 import androidx.recyclerview.widget.RecyclerView
+import com.afollestad.materialdialogs.MaterialDialog
 import com.bumptech.glide.Glide
 import com.google.firebase.auth.FirebaseAuth
 import com.google.firebase.auth.FirebaseUser
@@ -14,8 +17,13 @@ import com.overdrive.sedekahsampah.R
 import com.overdrive.sedekahsampah.models.ImageStorage
 import com.overdrive.sedekahsampah.models.Post
 import com.overdrive.sedekahsampah.ui.home.post.Interaction
+import com.overdrive.sedekahsampah.ui.home.post.InteractionEditClick
 import com.overdrive.sedekahsampah.ui.home.post.PostAdapter
+import com.overdrive.sedekahsampah.ui.home.post.PostInfo
+import com.overdrive.sedekahsampah.ui.home.post.komentar.KomentarBottomFragment
+import com.stfalcon.imageviewer.StfalconImageViewer
 import de.hdodenhof.circleimageview.CircleImageView
+import kotlinx.android.synthetic.main.activity_user.*
 
 class UserActivity : AppCompatActivity(), Interaction {
     override fun onItemSelected(position: Int, item: Post) {
@@ -23,15 +31,80 @@ class UserActivity : AppCompatActivity(), Interaction {
     }
 
     override fun onImageSelected(image: MutableList<ImageStorage>) {
-
+        StfalconImageViewer.Builder<ImageStorage>(this@UserActivity, image) { v, im ->
+            Glide.with(v).load(im.url).into(v)
+        }.show()
     }
 
     override fun onActionMoreSelected(position: Int, item: Post) {
+        if(FirebaseAuth.getInstance().currentUser!!.isAnonymous){
+            dialogAnymous()
+            return
+        }
+        goToMorePost(item)
+    }
+
+    private fun goToMorePost(item: Post) {
+        val newFragment = PostInfo(item,onThreadCallback)
+
+        // The device is using a large layout, so show the fragment as a dialog
+
+            newFragment.show(supportFragmentManager, "morePost")
+
 
     }
 
-    override fun onKomentarSelected(position: Int, item: Post) {
 
+    private val onThreadCallback: InteractionEditClick = object  : InteractionEditClick {
+        override fun onEditListerner(id: String, title: String?, content: String?) {
+            val docRef = FirebaseFirestore.getInstance().collection("post").document(id)
+            docRef.update(mapOf(
+                "title" to title,
+                "body" to content
+            )).addOnSuccessListener {
+                Toast.makeText(this@UserActivity , "Post Berubah " ,Toast.LENGTH_LONG).show()
+            }.addOnFailureListener {
+                shomethingError()
+
+            }
+        }
+
+        override fun onLaporListener(item: Post) {
+
+        }
+
+        override fun onDeleteListener(item: Post) {
+            FirebaseFirestore.getInstance().collection("post").document(item.id!!).delete()
+                .addOnSuccessListener {
+                    alertDialog()
+                }.addOnFailureListener {
+                shomethingError()
+
+            }
+        }
+    }
+
+        private fun dialogAnymous() {
+        MaterialDialog(this).show {
+            title(R.string.anymous)
+            message(R.string.silhakanLogin)
+            cornerRadius(16f)
+            positiveButton(text = "Iya"){
+                dismiss()
+
+
+            }
+            negativeButton(text = "Tidak"){
+                dismiss()
+            }
+        }
+    }
+
+    override fun onKomentarSelected(position: Int, item: Post) {
+        val bundleof = bundleOf(KomentarBottomFragment.POST_KOMENTAR to item)
+        val kb = KomentarBottomFragment()
+        kb.arguments = bundleof
+        kb.show(supportFragmentManager,"KomentarSheet")
     }
 
     private lateinit var  rcvView : RecyclerView
@@ -50,6 +123,9 @@ class UserActivity : AppCompatActivity(), Interaction {
         user?.let {
             try {
                 displayName.text = it.displayName
+                if(it.phoneNumber.toString().isNotBlank()){
+                    tv_value_noHp.text = it.phoneNumber
+                }
                 Glide.with(this@UserActivity).load(it.photoUrl).into(ci_pitchure)
 
             }catch (e : Exception){
@@ -85,6 +161,29 @@ class UserActivity : AppCompatActivity(), Interaction {
 
     private fun initRecyleview(mlistPost: List<Post>) {
         mAdaptaer.submitList(mlistPost)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        finish()
+        return super.onSupportNavigateUp()
+    }
+
+
+        private fun shomethingError(){
+            Toast.makeText(this@UserActivity,"Something is Error Please Try Again", Toast.LENGTH_LONG).show()
+        }
+
+
+    private fun alertDialog() {
+        MaterialDialog(this@UserActivity).show {
+            title(R.string.pemberitahuan)
+            message(R.string.terhapus)
+            cornerRadius(16f)
+            positiveButton(text = "Iya") {
+                dismiss()
+
+            }
+        }
     }
 
 }
