3cb8abcd0e04563cd2db24cb960589579151da29
Harloom <harloom.developer@gmail.com>
2019-12-24 08:42:29 +0700

    flash screen

---
+++ app/src/main/java/com/overdrive/sedekahsampah/ScrennActivity.kt
@@ -0,0 +1,12 @@
+package com.overdrive.sedekahsampah
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ScrennActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_screnn)
+    }
+}
